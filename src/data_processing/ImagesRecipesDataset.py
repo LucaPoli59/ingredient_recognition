dfa2fd8eadef56d140052f4ec408a1ae6f7c06a7
LucaPoli59 <poli.luca.marconi@gmail.com>
2024-04-30 14:15:46 +0200

    Refactor ImagesRecipesDataset by splitting the class in two: - base class: that takes processed data (images_paths and labels); - wrapper class: that makes the preprocessing and validation part

--- src/data_processing/ImagesRecipesDataset.py
+++ src/data_processing/ImagesRecipesDataset.py
@@ -2,64 +2,27 @@ import pathlib
 import json
 
 import pandas as pd
+from numpy import ndarray
 from torch.utils.data import Dataset
 from torchvision.transforms import v2
-from typing import Tuple
+from typing import Tuple, List, Dict, Iterable
 from PIL import Image
+from sklearn.base import TransformerMixin as any_sk_transformer
 
 from settings.config import *
 from settings.commons import *
 from src.data_processing.MultiLabelBinarizerRobust import MultiLabelBinarizerRobust
 
 
-class ImagesRecipesDataset(Dataset):
-    def __init__(self, image_dir, recipes_file, transform=None, category=None, label_encoder=None,
-                 recipe_feature_label="ingredients_ok"):
-        self.image_dir = image_dir
-        self.recipes_file = recipes_file
+class _ImagesRecipesDataset(Dataset):
+    """Base Dataset class for the images and labels. It loads the images and recipes data and applies the
+    transformations to the images."""
 
-        if transform is None:
-            transform = v2.Compose([v2.ToImage(), v2.Resize((224, 224)), v2.ToDtype(torch.float32, scale=True)])
+    def __init__(self, images_paths, label_data, transform):
+        self.images_paths = images_paths
+        self.label_data = label_data
         self.transform = transform
-
-        if label_encoder is None:
-            label_encoder = MultiLabelBinarizerRobust()  # default encoder
-        self.label_encoder = label_encoder
-
-        if category is not None:
-            category = category.lower()
-            if category not in FOOD_CATEGORIES:
-                raise ValueError(f'Invalid category: {category}')
-        self.category = category
-
-        self.feature_label = recipe_feature_label
-        self._compute_images_paths()
-        self._compute_recipes()
-
-    def _compute_images_paths(self):
-        if self.category is None:
-            self.images_paths = list(pathlib.Path(self.image_dir).glob('*.jpg'))
-        else:
-            self.images_paths = list(pathlib.Path(self.image_dir).glob(f'*{tokenize_category(self.category)}.jpg'))
-
-    def _compute_recipes(self):
-        if self.category is None:
-            self.recipes = json.load(open(self.recipes_file))
-        else:
-            recipes = json.load(open(self.recipes_file))
-            # Filter recipes by category chosen
-            self.recipes = list(filter(lambda recipe: recipe['cuisine'].lower() == self.category, recipes))
-            if not self.recipes:
-                raise ValueError(f"No recipes found for category {self.category}")
-
-        self._encode_label_data()
-
-    def _encode_label_data(self):
-        # Fit the encoder to the label feature if it is not already fitted, and then transform it
-        label_data_raw = pd.DataFrame(self.recipes)[self.feature_label].values
-        if not self.label_encoder.is_fitted():
-            self.label_encoder.fit(label_data_raw)
-        self.label_data = self.label_encoder.transform(label_data_raw)
+        super().__init__()
 
     def load_image(self, idx) -> Image.Image:
         """Opens an image via its path and returns it as a PIL image."""
@@ -76,3 +39,65 @@ class ImagesRecipesDataset(Dataset):
             image = self.transform(image)
 
         return image, torch.tensor(label, dtype=torch.float32)
+
+
+class ImagesRecipesDataset(_ImagesRecipesDataset):
+    """Dataset class wrapper for the base class _ImagesRecipesDataset.
+    It loads the images from a general directory and the recipes from a json file, filters them by category,
+    encodes the recipes to pass everything to the base class."""
+
+    def __init__(self, image_dir, recipes_file, transform=None, category=None, label_encoder=None,
+                 recipe_feature_label="ingredients_ok"):
+
+        # Check validity of parameters
+        if transform is None:
+            transform = v2.Compose([v2.ToImage(), v2.Resize((224, 224)), v2.ToDtype(torch.float32, scale=True)])
+        if label_encoder is None:
+            label_encoder = MultiLabelBinarizerRobust()  # default encoder
+        if category is not None:
+            category = category.lower()
+            if category not in FOOD_CATEGORIES:
+                raise ValueError(f'Invalid category: {category}')
+
+        # Compute images_path, Load recipes filter them by category and encode them to get the label data
+        images_paths, label_data, label_encoder = images_recipes_processing(image_dir, recipes_file, category,
+                                                                            label_encoder, recipe_feature_label)
+
+        super().__init__(images_paths, label_data, transform)
+
+
+def images_recipes_processing(images_dir: os.path, recipes_file: os.path, category: str | None = None,
+                              label_encoder: any_sk_transformer | MultiLabelBinarizerRobust = None,
+                              recipe_feature_label: str = "ingredients_ok"
+                              ) -> Tuple[List[pathlib.Path], ndarray, any_sk_transformer | MultiLabelBinarizerRobust]:
+    """Function that processes the images and recipes data, filtering them by category, encoding the recipes and
+    returning the images paths, the label data and the label encoder."""
+
+    images_paths = _compute_images_paths(images_dir, category)
+    recipes = json.load(open(recipes_file))
+    recipes = _recipes_filter_by_category(recipes, category)
+    label_data, label_encoder = _encode_recipes(recipes, label_encoder, recipe_feature_label)
+    return images_paths, label_data, label_encoder
+
+
+def _recipes_filter_by_category(recipes: List[Dict], category: str | None = None) -> List[Dict]:
+    if category is None or category == "all":
+        return recipes
+    return list(filter(lambda recipe: recipe['cuisine'].lower() == category, recipes))
+
+
+def _encode_recipes(recipes: List[Dict],
+                    label_encoder: any_sk_transformer | MultiLabelBinarizerRobust,
+                    feature_label: str) -> Tuple[ndarray, any_sk_transformer | MultiLabelBinarizerRobust]:
+    # Fit the encoder to the label feature if it is not already fitted, and then transform it
+    label_data_raw = pd.DataFrame(recipes)[feature_label].values
+    if not label_encoder.is_fitted():
+        label_encoder.fit(label_data_raw)
+    return label_encoder.transform(label_data_raw), label_encoder
+
+
+def _compute_images_paths(images_dir: os.path, category: str) -> List[pathlib.Path]:
+    if category is None:
+        return list(pathlib.Path(images_dir).glob('*.jpg'))
+    else:
+        return list(pathlib.Path(images_dir).glob(f'*{tokenize_category(category)}.jpg'))
